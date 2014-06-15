bf1492ae53d22b3f0fab6c8b2fb8a750fd5144b6
Saul Maddox <saul.maddox@gmail.com>
2014-06-15 13:23:15 -0500

    Update plugin for Hapi 2.0.0

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var Hoek = require('hoek'),
-    Path = require('path'),
+var Path = require('path'),
     _ = require('lodash');
 
-exports.register = function (plugin, options, callback) {
+exports.register = function (plugin, options, next) {
     // if options is not given, create version cache from package.json version. else use the options as the cache version
     if(_.isEmpty(options)){
         // Get the version of the application that loads this module.
-        var packageVersionNumber = Hoek.loadPackage(Path.join(__dirname, '../..')).version;
+        var packageVersionNumber = require(Path.join(__dirname, '../..') + "/package.json").version;
 
         // Replace all characters except numbers.
         var versionCache = packageVersionNumber.replace(/[^0-9]/g, "");
@@ -26,6 +25,11 @@ exports.register = function (plugin, options, callback) {
                 }
                 response.source.context.version.cache = '?v=' + versionCache;
         }
-        next();
+        return next();
     });
+    return next();
+};
+
+exports.register.attributes = {
+    pkg: require("./package.json")
 };
